e2e038c8e430e865dc24faa3a79dcbefc7af8667
Rafael Henrique <rafael@rah.com.br>
2023-04-14 16:42:14 -0300

    feat: adjust gitignore

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name='sqlalchemyfilter',
-    packages=find_packages(include=['sqlalchemyfilter']),
     version='0.0.1',
     description='Filtered/paginated lists on DTO classes using SQLAlchemy',
     author='Rah Henrique',
     author_email='rafael@rah.com.br',
+    url='https://github.com/rahenrique/sqlalchemy-filter',
+    packages=find_packages(include=['sqlalchemyfilter']),
     license='MIT',
 )
