6885e81da457c60652b1a16867f87216b6071748
Rafael Henrique <rafael@rah.com.br>
2023-05-18 11:35:28 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='SQLAlchemyFilter',
     version='0.0.1',
-    description='Filtered/paginated lists on DTO classes using SQLAlchemy',
+    description='Filtered/paginated lists on DAO classes using SQLAlchemy',
     author='Rah Henrique',
     author_email='rafael@rah.com.br',
     url='https://github.com/rahenrique/sqlalchemy-filter',
