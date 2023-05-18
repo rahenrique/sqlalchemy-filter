300e03845e97c61949effe6e1007e49aa5c06095
Rafael Henrique <rafael@rah.com.br>
2023-05-18 11:38:19 -0300

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='SQLAlchemyFilter',
-    version='0.0.1',
+    version='0.0.2',
     description='Filtered/paginated lists on DAO classes using SQLAlchemy',
     author='Rah Henrique',
     author_email='rafael@rah.com.br',
